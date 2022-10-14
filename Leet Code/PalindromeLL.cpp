a07dd815bba5e4191a07e46d58b79b1c5a7f8010
Avni Goyal <76475795+Avnigoyal22@users.noreply.github.com>
2022-10-15 01:29:09 +0530

    Add files via upload

---
+++ Leet Code/PalindromeLL.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+void insert_end(ListNode* &head, int val){
+    ListNode* n = new ListNode(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    ListNode* temp = head;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next = n;
+}
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(slow!=NULL && slow->next!=NULL){
+            fast=fast->next;
+            slow=slow->next->next;
+        }
+        
+        ListNode* z = NULL;
+        while(fast!=NULL){
+            ListNode* temp = fast->next;
+            fast->next = z;
+            z=fast;
+            fast = temp;
+        }
+
+        while(z){
+            
+            if(z->val!=head->val){
+                return false;
+            }
+            z=z->next;
+            head=head->next;
+        }
+        return true;
+    }
+};
+
+int main(){
+    ListNode* head;
+    insert_end(head, 10);
+    insert_end(head, 20);
+    insert_end(head, 30);
+    insert_end(head, 40);
+    insert_end(head, 20);
+    insert_end(head, 10);
+
+    Solution s;
+    
+    cout<<s.isPalindrome(head);
+}
