e2d44850bdd2233eaddfb71509420413fb45e975
Avni Goyal <76475795+Avnigoyal22@users.noreply.github.com>
2022-10-15 23:41:55 +0530

    Update RunningSumOf1DArray.cpp

--- Leet Code/RunningSumOf1DArray.cpp
+++ Leet Code/RunningSumOf1DArray.cpp
@@ -1,4 +1,4 @@
-
+// Approach 1
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
@@ -11,5 +11,18 @@ public:
         }
         
         return arr;
+    }
+};
+
+// Approach 2
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }
+        
+        return nums;
     }
 };
